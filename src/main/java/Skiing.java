9e53ab104c0573b27261a682fc34a5afb5ddaba5
alorthius <anna-alina.bondarets@ucu.edu.ua>
2021-11-17 15:18:48 +0200

    Make a bit faster

--- src/main/java/Skiing.java
+++ src/main/java/Skiing.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -10,21 +12,58 @@ public class Skiing {
         for (int index = 0; index < length; ++index) {
             path[index] = in.nextInt();
         }
+
+//        int length = 100000;
+//        Random rd = new Random();
+//        int[] path = new int[length];
+//
+//        for (int i = 0; i < length; ++i) {
+//            path[i] = rd.nextInt(2);
+//        }
+//
+//        long start = System.currentTimeMillis();
+//
         System.out.println(count(path));
+
+//        long end =System.currentTimeMillis();
+//        System.out.println((end - start) / 1000F);
     }
 
-    public static int count(int[] repetitions) {
+    public static int count(int[] path) {
+        int pathLength = path.length;
         int maxLength = 0;
-        for (int mainInd = 0; mainInd < repetitions.length; ++mainInd) {
-            int sum = (int) Math.pow(-1, repetitions[mainInd]);
-            int count = 1;
+        int[] tabulation = new int[pathLength];
 
-            for (int indToRight = mainInd + 1; indToRight < repetitions.length; ++indToRight) {
-                sum += (int) Math.pow(-1, repetitions[indToRight]);
-                count += 1;
-                if (sum == 0 && count > maxLength) {
-                    maxLength = count;
+        int s = 0;
+        for (int mainInd = 0; mainInd < pathLength; ++mainInd) {
+            s += Math.pow(-1, path[mainInd]);
+            tabulation[mainInd] = s;
+//            tab.put(mainInd, s);
+        }
+
+        for (int mainInd = 0; mainInd < pathLength; ++mainInd) {
+
+            for (int indToRight = pathLength - 1; indToRight > mainInd; --indToRight) {
+                int sum;
+
+                try {
+                    sum = tabulation[indToRight] - tabulation[mainInd - 1];
+//                    sum = tab.get(indToRight) - tab.get(mainInd - 1);
                 }
+//                catch (NullPointerException exception) {
+//                    sum = tab.get(indToRight);
+//                }
+                catch (ArrayIndexOutOfBoundsException exception) {
+                    sum = tabulation[indToRight];
+                }
+
+                if (sum == 0 && indToRight - mainInd + 1 > maxLength) {
+                    maxLength = indToRight - mainInd + 1;
+                    break;
+                }
+            }
+            if (maxLength >= pathLength - mainInd ) {
+                break;
             }
         }
         return maxLength;
