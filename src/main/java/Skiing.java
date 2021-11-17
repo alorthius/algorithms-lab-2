bc9aead04471a6bdcccb1f43a9a87b1178303a88
alorthius <anna-alina.bondarets@ucu.edu.ua>
2021-11-17 10:28:54 +0200

    Add correct yet too slow implementation

--- src/main/java/Skiing.java
+++ src/main/java/Skiing.java
@@ -1,23 +1,32 @@
 import java.util.Scanner;
 
+
 public class Skiing {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int length = in.nextInt();
-        String i = in.nextLine();
-        String[] input = in.nextLine().split(" ");
-        System.out.println(findChunkLength(input, length));
-    }
-
-    public static int findChunkLength(String[] array, int length) {
-        int ones = 0;
+        int[] path = new int[length];
 
         for (int index = 0; index < length; ++index) {
-            if (Integer.parseInt(array[index]) == 1) {
-                ++ones;
+            path[index] = in.nextInt();
+        }
+        System.out.println(count(path));
+    }
+
+    public static int count(int[] repetitions) {
+        int maxLength = 0;
+        for (int mainInd = 0; mainInd < repetitions.length; ++mainInd) {
+            int sum = (int) Math.pow(-1, repetitions[mainInd]);
+            int count = 1;
+
+            for (int indToRight = mainInd + 1; indToRight < repetitions.length; ++indToRight) {
+                sum += (int) Math.pow(-1, repetitions[indToRight]);
+                count += 1;
+                if (sum == 0 && count > maxLength) {
+                    maxLength = count;
+                }
             }
         }
-        int zeros = length - ones;
-        return Math.min(zeros, ones) * 2;
+        return maxLength;
     }
 }
