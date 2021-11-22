44a83c81aec2c9afaf04b2d2657d9e67b98fcfae
alorthius <anna-alina.bondarets@ucu.edu.ua>
2021-11-22 16:27:00 +0200

    Change 6th task implementation (still too slow)

--- src/main/java/DLLQueue.java
+++ src/main/java/DLLQueue.java
@@ -1,111 +1,75 @@
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 
 public class DLLQueue {
-    Node startingNode = null;
-    Node endingNode = null;
 
     public static void main(String[] args) {
-        DLLQueue q = new DLLQueue();
         Scanner in = new Scanner(System.in);
+        PriorityQueue<QItem> queue = new PriorityQueue<>();
+        PriorityQueue<QItem> queueReversed = new PriorityQueue<>(java.util.Collections.reverseOrder());
 
         while (in.hasNextLine()) {
             int command = in.nextInt();
             if (command == 0) {
                 break;
             }
+            else if (command == 1) {
+                pushItem(in.nextInt(), in.nextInt(), queue, queueReversed);
+            }
             else if (command == 2) {
-                System.out.println(q.popMaxPrior());
+                System.out.println(popMaxPrior(queue, queueReversed));
             }
             else if (command == 3) {
-                System.out.println(q.popMinPrior());
-            }
-            else {
-                q.pushItem(in.nextInt(), in.nextInt());
+                System.out.println(popMinPrior(queue, queueReversed));
             }
         }
     }
 
-    public void pushItem(int value, int priority) {
-        Node newNode = new Node(value, priority);
-
-        if (startingNode == null) {
-            endingNode = newNode;
-            startingNode = newNode;
-            return;
-        }
-        pushHelper(newNode);
+    public static void pushItem(int value, int priority, PriorityQueue<QItem> queue, PriorityQueue<QItem> queueReversed) {
+        queue.add(new QItem(value, priority));
+        queueReversed.add(new QItem(value, priority));
     }
 
-    private void pushHelper(Node newNode) {
-        if (this.startingNode == null) {
-            this.startingNode = newNode;
-            this.endingNode = newNode;
-        }
-        else if (this.startingNode.priority <= newNode.priority) {
-            newNode.next = this.startingNode;
-            newNode.next.previous = newNode;
-            this.startingNode = newNode;
-        }
-        else {
-            Node current = this.startingNode;
-            while (current.next != null && current.next.priority > newNode.priority) {
-                current = current.next;
-            }
-            newNode.next = current.next;
-
-            if (current.next != null) {
-                newNode.next.previous = newNode;
-            } else {
-                this.endingNode = newNode;
-            }
-            current.next = newNode;
-            newNode.previous = current;
-        }
-    }
-
-    public int popMaxPrior() {
-        if (this.startingNode == null) {
+    public static int popMaxPrior(PriorityQueue<QItem> queue, PriorityQueue<QItem> queueReversed) {
+        QItem popped;
+        try {
+            popped = queueReversed.poll();
+            queue.remove(popped);
+            return popped.value;
+        } catch (NullPointerException exception) {
             return 0;
         }
-        int value = this.startingNode.value;
-        this.startingNode = this.startingNode.next;
-        return value;
     }
 
-    public int popMinPrior() {
-        if (this.endingNode == null) {
+    public static int popMinPrior(PriorityQueue<QItem> queue, PriorityQueue<QItem> queueReversed) {
+        QItem popped;
+        try {
+            popped = queue.poll();
+            queueReversed.remove(popped);
+            return popped.value;
+        } catch (NullPointerException exception) {
             return 0;
         }
-        int value = this.endingNode.value;
-        this.endingNode.previous.next = null;
-        this.endingNode = null;
-        return value;
     }
 
-    public void printQueue() {
-        Node node = startingNode;
-        System.out.println(node);
+    public static class QItem implements Comparable<QItem> {
+        public int value;
+        public int priority;
 
-        while (node.next != null) {
-            node = node.next;
-            System.out.println(node);
-        }
-    }
-
-    public static class Node {
-        int value;
-        int priority;
-        Node previous = null;
-        Node next = null;
-
-        public Node(int value, int priority) {
+        public QItem(int value, int priority) {
             this.value = value;
             this.priority = priority;
         }
 
-        public String toString() {
-            return String.format("(%d: %d)%n", this.value, this.priority);
+        @Override
+        public int compareTo(QItem anotherItem) {
+            return Integer.compare(this.priority, anotherItem.priority);
+        }
+
+        @Override
+        public boolean equals(Object anotherItem) {
+            return this.value == ((QItem) anotherItem).value;
         }
     }
 }
